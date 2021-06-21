d212f6ba5adf211a5551e0e4a3ae138292120a38
hp <mona.olabz@gmail.com>
2021-06-21 12:07:15 +0200

    initial commit

---
+++ src/test/java/com/ATGAutomation/Tests/CreateCouponTest.java
@@ -0,0 +1,28 @@
+package com.ATGAutomation.Tests;
+
+import org.testng.annotations.Test;
+
+import com.ATGAutomation.Pages.HomePage;
+import com.aventstack.extentreports.Status;
+
+public class CreateCouponTest extends BaseTest{
+	@Test
+	public void createCoupon() throws InterruptedException
+	{
+		reportUtils.createATestCase("createCoupon");
+		reportUtils.addTestLog(Status.INFO, "performingLog");
+		HomePage homePage = new HomePage(driver);
+		homePage.acceptAlertCookies();
+		homePage.clickOnSpelaLink();
+		homePage.scrolDown();
+		homePage.clickOnV4Link();
+		homePage.clickOnV41Btn();
+		homePage.clickOnV42Btn();
+		homePage.clickOnV43Btn();
+		homePage.clickOnV4AllBtn();
+		homePage.clickOnNewCouponBtn();
+		homePage.clickOnBlankCouponBtn();
+	}
+
+
+}
