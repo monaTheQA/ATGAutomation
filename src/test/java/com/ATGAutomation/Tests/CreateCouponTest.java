ee83837f56c7a3a2a7c1bba6c656a3b43539782a
hp <mona.olabz@gmail.com>
2021-06-22 08:12:39 +0200

    updated

--- src/test/java/com/ATGAutomation/Tests/CreateCouponTest.java
+++ src/test/java/com/ATGAutomation/Tests/CreateCouponTest.java
@@ -13,8 +13,8 @@ public class CreateCouponTest extends BaseTest{
 	@Test
 	public void createCoupon() throws InterruptedException
 	{
-		//reportUtils.createATestCase("createCoupon");
-		//reportUtils.addTestLog(Status.INFO, "performingLog");
+		reportUtils.createATestCase("createCoupon");
+		reportUtils.addTestLog(Status.INFO, "performingLog");
 		HomePage homePage = new HomePage(driver);
 		System.out.println("HomePage");
 		homePage.acceptAlertCookies();	
